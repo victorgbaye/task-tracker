44f05af0f676bfc00ce0f6849340705a1335895a
tomiwadotJS <victorgbayedev@gmail.com>
2025-05-11 22:15:29 +0100

    craete task and get all task handler

--- cmd/api/tasks.go
+++ cmd/api/tasks.go
@@ -1,13 +1,43 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Task struct{
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Done    bool   `json:"done"`
+}
+
+var tasks = []Task{}
+var nextID = 1
 
 func (app application) createTask(w http.ResponseWriter, r *http.Request)  {
-	
+	var task Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	task.ID = nextID
+	nextID++
+	tasks = append(tasks, task)
 }
+
 func (app application) getAllTask(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("hello world"))
+	w.Header().Set("Content-Type", "application/json")
+	
+	err:= json.NewEncoder(w).Encode(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
+
 func (app application) getTask(w http.ResponseWriter, r *http.Request)  {
 	
 }
